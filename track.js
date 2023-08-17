186451f7206bf13934739be2a6dca5f21119e123
Myles Wirth <regime_skylark.0y@icloud.com>
2023-08-17 16:39:54 -0400

    Updated images and fixed slider
    Now "next-gen"

--- track.js
+++ track.js
@@ -13,7 +13,7 @@ const handleOnUp = () => {
 const handleOnMove = e => {
   if (track.dataset.mouseDownAt === "0") return;
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-  const maxDelta = window.innerWidth / 2;
+  const maxDelta = window.innerWidth * 1.5;
   const percentage = (mouseDelta / maxDelta) * -100;
   const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
   const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
@@ -38,79 +38,3 @@ window.onmouseup = e => handleOnUp(e);
 window.ontouchend = e => handleOnUp(e.touches[0]);
 window.onmousemove = e => handleOnMove(e);
 window.ontouchmove = e => handleOnMove(e.touches[0]);
-
-
-const slider = document.querySelector('.slider');
-const sliderContainer = document.querySelector('.sliderContainer');
-const items = document.querySelectorAll('.item');
-let currentIndex = 0;
-let touchStartX = 0;
-
-// Calculate the total width of all items
-const totalItemsWidth = Array.from(items).reduce((total, item) => {
-    const itemStyle = getComputedStyle(item);
-    const itemWidth = item.offsetWidth + parseFloat(itemStyle.marginLeft) + parseFloat(itemStyle.marginRight);
-    return total + itemWidth;
-  }, 0);
-
-// Set the width of the slider container to accommodate all items
-sliderContainer.style.width = `${totalItemsWidth}px`;
-
-function slideTo(index) {
-const itemStyle = getComputedStyle(items[index]);
-  const itemMarginLeft = parseFloat(itemStyle.marginLeft);
-  const itemMarginRight = parseFloat(itemStyle.marginLeft);
-  let translation;
-  if (index < 0 || index >= items.length) {
-    return;
-  }
-
-  if (index == 1) {
-    translation = index * items[0].offsetWidth + (itemMarginLeft * 1.9);
-  }
-
-  if (index == 2) {
-    translation = index * items[0].offsetWidth + (itemMarginLeft + (itemMarginRight * 3));
-  }
-
-
-  
-  console.log(translation + "Margin-left" + itemMarginLeft);
-
-  sliderContainer.style.transform = `translateX(-${translation}px)`;
-  currentIndex = index;
-}
-
-function slideNext() {
-  slideTo(currentIndex + 1);
-}
-
-function slidePrev() {
-  slideTo(currentIndex - 1);
-}
-
-setInterval(slideNext, 4500); // Automatically slide every 3 seconds
-
-slider.addEventListener('touchstart', handleTouchStart);
-slider.addEventListener('touchmove', handleTouchMove);
-slider.addEventListener('touchend', handleTouchEnd);
-
-function handleTouchStart(event) {
-  touchStartX = event.touches[0].clientX;
-}
-
-function handleTouchMove(event) {
-  event.preventDefault(); // Prevent default scrolling behavior
-  const touchX = event.touches[0].clientX;
-  const touchDiff = touchX - touchStartX;
-
-  if (touchDiff > 50) {
-    slidePrev();
-  } else if (touchDiff < -50) {
-    slideNext();
-  }
-}
-
-function handleTouchEnd() {
-  touchStartX = 0;
-}
